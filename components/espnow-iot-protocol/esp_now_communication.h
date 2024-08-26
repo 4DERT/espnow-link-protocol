7ccbd3022c428ae3bdbb91c1c6fbad1666395587
4DERT <4dertdk@gmail.com>
2024-08-26 10:49:16 +0200

    Implement ESP-NOW communication module with send/receive functionality
    - Added `esp_now_communication.c` and `esp_now_communication.h` to implement ESP-NOW communication for IoT devices.
    - Configured Wi-Fi and ESP-NOW in `enc_init()` to initialize the ESP-NOW protocol.
    - Implemented `enc_send_with_result()` and `enc_send_no_result()` for sending data with and without acknowledgment.
    - Added tasks for handling ESP-NOW data sending and receiving, using FreeRTOS queues for managing data flow.
    - Updated `CMakeLists.txt` to include the new source file and added necessary dependencies like `esp_wifi` and `nvs_flash`.
    - Set up callback functions for handling data transmission status and receiving incoming data.

---
+++ components/espnow-iot-protocol/esp_now_communication.h
@@ -0,0 +1,37 @@
+#ifndef ESP_NOW_COMMUNICATION_H_
+#define ESP_NOW_COMMUNICATION_H_
+
+#include "esp_now.h"
+#include "freertos/FreeRTOS.h"
+
+#define ENC_CHANNEL 1
+#define ENC_SEND_QUEUE_SIZE 5
+#define ENC_RECIEVE_QUEUE_SIZE 5
+#define ENC_RESULT_QUEUE_SIZE 2
+
+typedef union {
+  uint8_t bytes[6];
+  uint64_t value;
+} enc_mac_t;
+
+typedef struct {
+  char data[ESP_NOW_MAX_DATA_LEN];
+  enc_mac_t dest_mac;
+
+  // private
+  QueueHandle_t* _ack_queue;
+} enc_send_t;
+
+typedef struct {
+  esp_now_recv_info_t esp_now_info;
+  char data[ESP_NOW_MAX_DATA_LEN];
+  int data_len;
+} enc_event_receive_cb_t;
+
+extern const enc_mac_t esp_now_broadcast_mac;
+
+void enc_init();
+bool enc_send_with_result(enc_send_t *data);
+void enc_send_no_result(enc_send_t *data);
+
+#endif // ESP_NOW_COMMUNICATION_H_
